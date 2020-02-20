16a598fe76362e5d6c872f8cee561caaf0303e46
dario mambro <dario.mambro@gmail.com>
2020-02-20 15:56:41 +0100

    added GammaEnvEditor

---
+++ GammaEnvEditor.cpp
@@ -0,0 +1,70 @@
+/*
+Copyright 2020 Dario Mambro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "GammaEnvEditor.h"
+
+GammaEnvEditor::GammaEnvEditor(AudioProcessorValueTreeState& apvts,
+                               GammaEnvParameters& parameters,
+                               String const& midSideParamID)
+  : attack(apvts, "Attack", parameters.attack)
+  , release(apvts, "Release", parameters.release)
+  , releaseDelay(apvts, "Release Delay", parameters.releaseDelay)
+  , attackDelay(apvts, "Attack Delay", parameters.attackDelay)
+  , metric(apvts, "Metric", { "Peak", "RMS" }, parameters.metric)
+  , channelLabels(apvts, midSideParamID)
+{
+  addAndMakeVisible(attack);
+  addAndMakeVisible(release);
+  addAndMakeVisible(releaseDelay);
+  addAndMakeVisible(attackDelay);
+  addAndMakeVisible(metric);
+  addAndMakeVisible(channelLabels);
+
+  setSize(WIDTH, 120.f);
+  setOpaque(false);
+}
+
+void
+GammaEnvEditor::resized()
+{
+  int left = 0;
+
+  auto const resize = [&](auto& component, int width) {
+    component.setTopLeftPosition(left, 0.f);
+    component.setSize(width, getHeight());
+    left += width - 1;
+  };
+
+  resize(channelLabels, 50);
+
+  resize(metric, 90);
+  resize(attack, 140);
+  resize(release, 140);
+  resize(attackDelay, 140);
+  resize(releaseDelay, 140);
+}
+
+void
+GammaEnvEditor::setTableSettings(LinkableControlTable tableSettings)
+{
+  channelLabels.tableSettings = tableSettings;
+  metric.tableSettings = tableSettings;
+  attack.tableSettings = tableSettings;
+  release.tableSettings = tableSettings;
+  attackDelay.tableSettings = tableSettings;
+  releaseDelay.tableSettings = tableSettings;
+}
---
+++ GammaEnvEditor.h
@@ -0,0 +1,54 @@
+/*
+Copyright 2020 Dario Mambro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "Attachments.h"
+#include "Linkables.h"
+#include <JuceHeader.h>
+#include <array>
+
+struct GammaEnvParameters
+{
+  LinkableParameter<AudioParameterFloat> attack;
+  LinkableParameter<AudioParameterFloat> release;
+  LinkableParameter<AudioParameterFloat> attackDelay;
+  LinkableParameter<AudioParameterFloat> releaseDelay;
+  LinkableParameter<AudioParameterChoice> metric;
+};
+
+class GammaEnvEditor : public Component
+{
+public:
+  static constexpr int WIDTH = 140 * 4 + 90 + 50;
+
+  GammaEnvEditor(AudioProcessorValueTreeState& apvts,
+                 GammaEnvParameters& parameters,
+                 String const& midSideParamID = "Mid-Side");
+
+  void resized() override;
+
+  void setTableSettings(LinkableControlTable tableSettings);
+
+private:
+  ChannelLabels channelLabels;
+
+  LinkableComboBox metric;
+  LinkableControl<AttachedSlider> attack;
+  LinkableControl<AttachedSlider> release;
+  LinkableControl<AttachedSlider> attackDelay;
+  LinkableControl<AttachedSlider> releaseDelay;
+};
