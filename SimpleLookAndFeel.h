9642d5e06d75c4543deb779191110bc92745df89
dario mambro <dario.mambro@gmail.com>
2020-02-21 10:36:37 +0100

    fixed typo

--- SimpleLookAndFeel.h
+++ SimpleLookAndFeel.h
@@ -14,7 +14,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#include < JuceHeader.h>
+#include <JuceHeader.h>
 
 class SimpleLookAndFeel : public LookAndFeel_V4
 {
